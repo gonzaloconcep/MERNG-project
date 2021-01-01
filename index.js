29b26298b671ae372eb8c3b5684486916b27695c
akuatiko95 <lasconchasgonzalo@gmail.com>
2021-01-01 19:59:44 +0100

    Initial commit - Register feature

---
+++ index.js
@@ -0,0 +1,21 @@
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const { MONGODB } = require("./config");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((res) => {
+    console.log("MongoDB connected");
+    server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running http://localhost:5000`);
+  });
