cf0a763fb9184bc041e0ef2cedbd6874200fb1ae
akuatiko95 <lasconchasgonzalo@gmail.com>
2021-01-03 18:17:00 +0100

    Likes and comments counters
    Check body empty on createPost

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,6 +3,11 @@ const usersResolvers = require("./users");
 const commentsResolvers = require("./comments");
 
 module.exports = {
+  // Post modifier (every mutation, query or subscription that returns a post go throw this)
+  Post: {
+    likeCount: (parent) => parent.likes.length,
+    commentCount: (parent) => parent.comments.length,
+  },
   Query: {
     ...postsResolvers.Query,
   },
