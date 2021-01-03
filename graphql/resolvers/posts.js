e525cdaa60b487be651f105980b2092f4354a3d2
akuatiko95 <lasconchasgonzalo@gmail.com>
2021-01-03 14:18:38 +0100

    Subcription on create new posts

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -37,6 +37,10 @@ module.exports = {
       });
       const post = await newPost.save();
 
+      context.pubsub.publish("NEW_POST", {
+        newPost: post,
+      });
+
       return post;
     },
     async deletePost(_, { postId }, context) {
@@ -73,6 +77,11 @@ module.exports = {
         await post.save();
         return post;
       } else throw new UserInputError("Post not found");
+    },
+  },
+  Subscription: {
+    newPost: {
+      subscribe: (_, __, { pubsub }) => pubsub.asyncIterator("NEW_POST"),
     },
   },
 };
