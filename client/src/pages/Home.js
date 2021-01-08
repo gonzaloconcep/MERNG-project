c5ece58a1ab81662b559241b59383dbd31379f0c
akuatiko95 <lasconchasgonzalo@gmail.com>
2021-01-08 19:26:14 +0100

    Displaying posts

---
+++ client/src/pages/Home.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
+import { Grid, GridColumn } from "semantic-ui-react";
+
+import PostCard from "../components/PostCard";
+function Home() {
+  const {
+    loading,
+    data: { getPosts: posts },
+  } = useQuery(FETCH_POSTS_QUERY);
+
+  return (
+    <Grid columns={3}>
+      <Grid.Row className="page-title">
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <h1>Loading posts..</h1>
+        ) : (
+          posts &&
+          posts.map((post) => (
+            <GridColumn key={post.id} style={{ margiBottom: 20 }}>
+              <PostCard post={post} />
+            </GridColumn>
+          ))
+        )}
+      </Grid.Row>
+    </Grid>
+  );
+}
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
+
+export default Home;
